ca8a9793c1e1e639028eaacf52317315f84ace5c
Martin Wilck <mwilck@suse.com>
2017-07-17 22:21:55 +0200

    cut_nvme_local.sh: remove unnecessary dracut modules
    Reviewed-by: David Disseldorp <ddiss@suse.de>

--- cut_nvme_local.sh
+++ cut_nvme_local.sh
@@ -25,6 +25,6 @@ _rt_require_lib "libkeyutils.so.1"
 	--include "$RAPIDO_DIR/rapido.conf" "/rapido.conf" \
 	--include "$RAPIDO_DIR/vm_autorun.env" "/vm_autorun.env" \
 	--add-drivers "nvme-core nvme-fabrics nvme-loop nvmet zram lzo" \
-	--modules "bash base network ifcfg" \
+	--modules "bash base" \
 	$DRACUT_EXTRA_ARGS \
 	$DRACUT_OUT
