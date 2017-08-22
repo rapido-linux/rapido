1028f88ee6d2450b5a23b16f718aef1d7aba5a51
David Disseldorp <ddiss@suse.de>
2017-08-22 20:06:26 +0200

    samba_cephfs: add cut and autorun scripts
    Samba is deployed from source (via a new SAMBA_SRC rapido.conf
    parameter) and is plumbed into the running Ceph vstart cluster using
    the Samba ceph VFS module.
    A share is provisioned exporting the root of the preexisting CephFS
    filesystem. Credentials and share details match the existing CIFS_X
    rapido.conf parameters.
    
    Signed-off-by: David Disseldorp <ddiss@suse.de>

---
+++ cut_samba_cephfs.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# Copyright (C) SUSE LINUX GmbH 2017, all rights reserved.
+#
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation; either version 2.1 of the License, or
+# (at your option) version 3.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+# License for more details.
+
+RAPIDO_DIR="$(realpath -e ${0%/*})"
+. "${RAPIDO_DIR}/runtime.vars"
+
+_rt_require_ceph
+_rt_require_samba
+_rt_require_dracut_args
+_rt_require_lib "libssl3.so libsmime3.so libstdc++.so.6 libsoftokn3.so \
+		 libfreeblpriv3.so"	# NSS_InitContext() fails without
+
+"$DRACUT" --install "tail blockdev ps rmdir resize dd vim grep find df sha256sum \
+		   strace mkfs mkfs.xfs \
+		   which perl awk bc touch cut chmod true false \
+		   fio getfattr setfattr chacl attr killall sync \
+		   id sort uniq date expr tac diff head dirname seq \
+		   ${SAMBA_SRC}/bin/default/source3/utils/smbpasswd \
+		   ${SAMBA_SRC}/bin/modules/vfs/ceph.so \
+		   ${SAMBA_SRC}/bin/default/source3/smbd/smbd \
+		   $LIBS_INSTALL_LIST" \
+	--include "$CEPH_COMMON_LIB" "/usr/lib64/libceph-common.so.0" \
+	--include "$CEPHFS_LIB" "/usr/lib64/libcephfs.so.2" \
+	--include "$CEPH_CONF" "/etc/ceph/ceph.conf" \
+	--include "$CEPH_KEYRING" "/etc/ceph/keyring" \
+	--include "$RAPIDO_DIR/samba_cephfs_autorun.sh" "/.profile" \
+	--include "$RAPIDO_DIR/rapido.conf" "/rapido.conf" \
+	--include "$RAPIDO_DIR/vm_autorun.env" "/vm_autorun.env" \
+	--modules "bash base network ifcfg" \
+	$DRACUT_EXTRA_ARGS \
+	$DRACUT_OUT || _fail "dracut failed"
+
+# assign more memory
+_rt_xattr_vm_resources_set "$DRACUT_OUT" "2" "1024M"
---
+++ samba_cephfs_autorun.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+#
+# Copyright (C) SUSE LINUX GmbH 2017, all rights reserved.
+#
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation; either version 2.1 of the License, or
+# (at your option) version 3.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+# License for more details.
+
+if [ ! -f /vm_autorun.env ]; then
+	echo "Error: autorun scripts must be run from within an initramfs VM"
+	exit 1
+fi
+
+. /vm_autorun.env
+
+set -x
+
+# use a non-configurable UID/GID for now
+cifs_xid="579120"
+echo "${CIFS_USER}:x:${cifs_xid}:${cifs_xid}:Samba user:/:/sbin/nologin" \
+	>> /etc/passwd
+echo "${CIFS_USER}:x:${cifs_xid}:" >> /etc/group
+
+cat /proc/mounts | grep debugfs &> /dev/null
+if [ $? -ne 0 ]; then
+	mount -t debugfs debugfs /sys/kernel/debug/
+fi
+
+cat /proc/mounts | grep configfs &> /dev/null
+if [ $? -ne 0 ]; then
+	mount -t configfs configfs /sys/kernel/config/
+fi
+
+for i in $DYN_DEBUG_MODULES; do
+	echo "module $i +pf" > /sys/kernel/debug/dynamic_debug/control || _fatal
+done
+for i in $DYN_DEBUG_FILES; do
+	echo "file $i +pf" > /sys/kernel/debug/dynamic_debug/control || _fatal
+done
+
+sed -i "s#keyring = .*#keyring = /etc/ceph/keyring#g; \
+	s#admin socket = .*##g; \
+	s#run dir = .*#run dir = /var/run/#g; \
+	s#log file = .*#log file = /var/log/\$name.\$pid.log#g" \
+	/etc/ceph/ceph.conf
+
+mkdir -p /usr/local/samba/var/
+mkdir -p /usr/local/samba/etc/
+mkdir -p /usr/local/samba/var/lock
+mkdir -p /usr/local/samba/private/
+mkdir -p /usr/local/samba/lib/
+ln -s ${SAMBA_SRC}/bin/modules/vfs/ /usr/local/samba/lib/vfs
+
+cat > /usr/local/samba/etc/smb.conf << EOF
+[global]
+	workgroup = MYGROUP
+
+[${CIFS_SHARE}]
+	path = /
+	vfs objects = ceph
+	ceph: config_file = /etc/ceph/ceph.conf
+	ceph: user_id = $CEPH_USER
+	read only = no
+EOF
+
+${SAMBA_SRC}/bin/default/source3/smbd/smbd || _fatal
+
+set +x
+
+echo -e "${CIFS_PW}\n${CIFS_PW}\n" \
+	| ${SAMBA_SRC}/bin/default/source3/utils/smbpasswd -a $CIFS_USER -s \
+				|| _fatal
+
+ip link show eth0 | grep $MAC_ADDR1 &> /dev/null
+if [ $? -eq 0 ]; then
+	echo "Samba share ready at: //${IP_ADDR1}/${CIFS_SHARE}/"
+fi
+ip link show eth0 | grep $MAC_ADDR2 &> /dev/null
+if [ $? -eq 0 ]; then
+	echo "Samba share ready at: //${IP_ADDR2}/${CIFS_SHARE}/"
+fi
+echo "Log at: /usr/local/samba/var/log.smbd"
