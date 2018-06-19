a7a29789abaa7490844817312fd667232b40e73f
David Disseldorp <ddiss@suse.de>
2018-06-19 13:24:12 +0200

    X_rbd_autorun.sh: use common rbd map helper function
    Drop a heap of duplicate code.
    
    Link: https://github.com/rapido-linux/rapido/issues/50
    Signed-off-by: David Disseldorp <ddiss@suse.de>

--- lio_rbd_autorun.sh
+++ lio_rbd_autorun.sh
@@ -21,31 +21,11 @@ fi
 
 set -x
 
+# map rbd device
+_vm_ar_rbd_map
+
 # this path is reliant on the rbd udev rule to setup the link
 CEPH_RBD_DEV=/dev/rbd/${CEPH_RBD_POOL}/${CEPH_RBD_IMAGE}
-
-# start udevd, otherwise rbd hangs in wait_for_udev_add()
-ps -eo args | grep -v grep | grep /usr/lib/systemd/systemd-udevd \
-	|| /usr/lib/systemd/systemd-udevd --daemon
-
-# map rbd device
-_ini_parse "/etc/ceph/keyring" "client.${CEPH_USER}" "key"
-[ -z "$key" ] && _fatal "client.${CEPH_USER} key not found in keyring"
-if [ -z "$CEPH_MON_NAME" ]; then
-	# pass global section and use mon_host
-	_ini_parse "/etc/ceph/ceph.conf" "global" "mon_host"
-	MON_ADDRESS="$mon_host"
-else
-	_ini_parse "/etc/ceph/ceph.conf" "mon.${CEPH_MON_NAME}" "mon_addr"
-	MON_ADDRESS="$mon_addr"
-fi
-
-echo -n "$MON_ADDRESS name=${CEPH_USER},secret=$key \
-	 $CEPH_RBD_POOL $CEPH_RBD_IMAGE -" > /sys/bus/rbd/add \
-	 || _fatal
-udevadm settle || _fatal
-
-# confirm that udev brought up the $pool/$img device path link
 [ -L $CEPH_RBD_DEV ] || _fatal
 
 # mount configfs first
