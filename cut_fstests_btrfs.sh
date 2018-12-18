65b8d1b446f0a0fa023b67e7b82dcb2e3bb28382
David Disseldorp <ddiss@suse.de>
2018-12-18 14:26:08 +0100

    fstests_btrfs: add cut and autorun scripts
    These are pretty much the same as fstests_xfs, except that 4 SCRATCH
    zram devices are provisioned (for RAID6 testing). As a result, the
    VM memory allocation is also increased from 2G to 4G.
    
    Signed-off-by: David Disseldorp <ddiss@suse.de>

---
+++ cut_fstests_btrfs.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# Copyright (C) SUSE LINUX GmbH 2018, all rights reserved.
+#
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation; either version 2.1 of the License, or
+# (at your option) version 3.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+# License for more details.
+
+RAPIDO_DIR="$(realpath -e ${0%/*})"
+. "${RAPIDO_DIR}/runtime.vars"
+
+_rt_require_dracut_args
+_rt_require_fstests
+
+"$DRACUT" --install "tail blockdev ps rmdir resize dd vim grep find df sha256sum \
+		   strace mkfs mkfs.btrfs btrfs \
+		   which perl awk bc touch cut chmod true false unlink \
+		   mktemp getfattr setfattr chacl attr killall hexdump sync \
+		   id sort uniq date expr tac diff head dirname seq \
+		   basename tee egrep \
+		   fstrim fio logger dmsetup chattr lsattr cmp stat \
+		   dbench /usr/share/dbench/client.txt hostname getconf md5sum \
+		   od wc getfacl setfacl tr xargs sysctl link truncate quota \
+		   repquota setquota quotacheck quotaon pvremove vgremove \
+		   xfs_mkfile xfs_db xfs_io \
+		   chgrp du fgrep pgrep tar rev kill \
+		   ${FSTESTS_SRC}/ltp/* ${FSTESTS_SRC}/src/* \
+		   ${FSTESTS_SRC}/src/log-writes/* \
+		   ${FSTESTS_SRC}/src/aio-dio-regress/*" \
+	--include "$FSTESTS_SRC" "$FSTESTS_SRC" \
+	--include "$RAPIDO_DIR/fstests_btrfs_autorun.sh" "/.profile" \
+	--include "$RAPIDO_DIR/rapido.conf" "/rapido.conf" \
+	--include "$RAPIDO_DIR/vm_autorun.env" "/vm_autorun.env" \
+	--add-drivers "zram lzo dm-snapshot dm-flakey btrfs raid6_pq" \
+	--modules "bash base" \
+	$DRACUT_EXTRA_ARGS \
+	$DRACUT_OUT || _fail "dracut failed"
+
+_rt_xattr_vm_networkless_set "$DRACUT_OUT"
+# need enough memory for five 1G zram devices
+_rt_xattr_vm_resources_set "$DRACUT_OUT" "2" "4096M"
---
+++ fstests_btrfs_autorun.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+#
+# Copyright (C) SUSE LINUX GmbH 2018, all rights reserved.
+#
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation; either version 2.1 of the License, or
+# (at your option) version 3.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+# License for more details.
+
+if [ ! -f /vm_autorun.env ]; then
+	echo "Error: autorun scripts must be run from within an initramfs VM"
+	exit 1
+fi
+
+. /vm_autorun.env
+
+set -x
+
+hostname_fqn="`cat /proc/sys/kernel/hostname`" || _fatal "hostname unavailable"
+hostname_short="${hostname_fqn%%.*}"
+filesystem="btrfs"
+
+# need hosts file for hostname -s
+cat > /etc/hosts <<EOF
+127.0.0.1	$hostname_fqn	$hostname_short
+EOF
+
+# use a 4-dev scratch pool for btrfs
+num_devs="5"
+modprobe zram num_devices="${num_devs}" || _fatal "failed to load zram module"
+
+_vm_ar_dyn_debug_enable
+
+for i in $(seq 0 $((num_devs - 1))); do
+	echo "1G" > /sys/block/zram${i}/disksize \
+		|| _fatal "failed to set zram disksize"
+done
+
+mkdir -p /mnt/test
+mkdir -p /mnt/scratch
+
+mkfs.${filesystem} /dev/zram0 || _fatal "mkfs failed"
+mount -t $filesystem /dev/zram0 /mnt/test || _fatal
+# xfstests can handle scratch mkfs+mount
+
+[ -n "${FSTESTS_SRC}" ] || _fatal "FSTESTS_SRC unset"
+[ -d "${FSTESTS_SRC}" ] || _fatal "$FSTESTS_SRC missing"
+
+cfg="${FSTESTS_SRC}/configs/$(hostname -s).config"
+cat > $cfg << EOF
+MODULAR=0
+TEST_DIR=/mnt/test
+TEST_DEV=/dev/zram0
+SCRATCH_MNT=/mnt/scratch
+SCRATCH_DEV_POOL="/dev/zram1 /dev/zram2 /dev/zram3 /dev/zram4"
+EOF
+
+set +x
+
+echo "$filesystem filesystem ready for FSQA"
+
+if [ -n "$FSTESTS_AUTORUN_CMD" ]; then
+	cd ${FSTESTS_SRC} || _fatal
+	eval "$FSTESTS_AUTORUN_CMD"
+fi
