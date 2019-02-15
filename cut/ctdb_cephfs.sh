c9b22e90989eb638e9ebebe7dad23d26ff335ffa
Luis Henriques <lhenriques@suse.com>
2019-02-15 15:47:05 +0000

    ctdb_cephfs: use ceph-conf parser to get ceph configuration
    Migrate the ctdb_cephfs scripts to use the new _rt_write_ceph_config
    function, which uses the ceph-conf tool.
    
    Signed-off-by: Luis Henriques <lhenriques@suse.com>

--- cut/ctdb_cephfs.sh
+++ cut/ctdb_cephfs.sh
@@ -15,7 +15,12 @@
 RAPIDO_DIR="$(realpath -e ${0%/*})/.."
 . "${RAPIDO_DIR}/runtime.vars"
 
+vm_ceph_conf="$(mktemp --tmpdir vm_ceph_conf.XXXXX)"
+# remove tmp file once we're done
+trap "rm $vm_ceph_conf" 0 1 2 3 15
+
 _rt_require_ceph
+_rt_write_ceph_config $vm_ceph_conf
 _rt_require_samba_ctdb
 _rt_require_dracut_args
 _rt_require_lib "libssl3.so libsmime3.so libstdc++.so.6 libsoftokn3.so \
@@ -66,6 +71,7 @@ fi
 	--include "$RAPIDO_DIR/autorun/ctdb_cephfs.sh" "/.profile" \
 	--include "$RAPIDO_DIR/rapido.conf" "/rapido.conf" \
 	--include "$RAPIDO_DIR/vm_autorun.env" "/vm_autorun.env" \
+	--include "$vm_ceph_conf" "/vm_ceph.env" \
 	--modules "bash base network ifcfg" \
 	$DRACUT_EXTRA_ARGS \
 	$DRACUT_OUT || _fail "dracut failed"
