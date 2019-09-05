4db29a0fdf3477b9bf73243e0f759994f3f11134
David Disseldorp <ddiss@suse.de>
2019-09-05 15:23:28 +0200

    tools/zram_setup: use zram_hot_add.sh helper
    Signed-off-by: David Disseldorp <ddiss@suse.de>

--- tools/zram_setup.sh
+++ tools/zram_setup.sh
@@ -12,7 +12,7 @@
 # or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
 # License for more details.
 #
-# provision and mount zram compressed ramdisks
+# provision and mount a zram compressed ramdisk under "${RAPIDO_DIR}/initrds"
 
 RAPIDO_DIR="$(realpath -e ${0%/*})/.."
 . "${RAPIDO_DIR}/runtime.vars"
@@ -23,14 +23,10 @@ ZRAM_INITRD_MNT="${RAPIDO_DIR}/initrds"
 ZRAM_BACKUP_DIR=`mktemp --tmpdir -d zram_backup.XXXXXXXXXX` || _fail
 trap "rm -f ${ZRAM_BACKUP_DIR}/readme.txt && rmdir $ZRAM_BACKUP_DIR" 0 1 2 3 15
 
-set -x
-
 function _zram_setup() {
-	local zram_name=$1
-	local zram_size=$2
-	local zram_mnt=$3
-	local zram_mnt_owner=$4
-	local zram_dev="/dev/${zram_name}"
+	local zram_size=$1
+	local zram_mnt=$2
+	local zram_mnt_owner=$3
 
 	if [ -d $zram_mnt ]; then
 		mountpoint -q $zram_mnt && _fail "$zram_mnt already mounted"
@@ -39,23 +35,21 @@ function _zram_setup() {
 		chown $zram_mnt_owner $zram_mnt || _fail
 	fi
 
-	echo "${zram_size}" > /sys/block/$zram_name/disksize || _fail
+	local zram_dev="$(${RAPIDO_DIR}/tools/zram_hot_add.sh \
+				${zram_size} ${zram_mnt_owner})"
+	[ -b "$zram_dev" ] || _fail
 
 	mkfs.xfs $zram_dev || _fail
 
 	mount $zram_dev $zram_mnt || _fail
 	chown $zram_mnt_owner $zram_mnt || _fail
 
-	echo "mounted $zram_name for $zram_mnt_owner at $zram_mnt"
+	echo "mounted $zram_dev for $zram_mnt_owner at $zram_mnt"
 }
-
-num_zram_devs=1
-modprobe zram num_devices="${num_zram_devs}" || _fail
-zram_i=0
 
 # backup readme.txt before it's mounted over, so that git doesn't detect removal
 cp -p ${ZRAM_INITRD_MNT}/readme.txt ${ZRAM_BACKUP_DIR}/
 # use rapido dir ownership for initramfs subdir mount point
 owner=`stat --format="%U:%G" $RAPIDO_DIR` || _fail
-_zram_setup "zram${zram_i}" $ZRAM_INITRD_SIZE $ZRAM_INITRD_MNT $owner
+_zram_setup $ZRAM_INITRD_SIZE $ZRAM_INITRD_MNT $owner
 cp -p ${ZRAM_BACKUP_DIR}/readme.txt ${ZRAM_INITRD_MNT}/
