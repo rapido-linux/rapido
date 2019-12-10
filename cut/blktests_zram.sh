db9ffe553315925d4791d01ebc1446c5f41697c8
Daniel Wagner <dwagner@suse.de>
2019-12-10 15:29:45 +0100

    cut: support NVME block tests
    To run the blktest the NVME modules are necessary. Also add the
    userland tool to setup and control NVME devices.
    
    Signed-off-by: Daniel Wagner <dwagner@suse.de>

--- cut/blktests_zram.sh
+++ cut/blktests_zram.sh
@@ -23,12 +23,12 @@ _rt_require_blktests
 		   lsblk strace which awk bc touch cut chmod true false mktemp \
 		   killall id sort uniq date expr tac diff head dirname seq \
 		   basename tee egrep hexdump sync fio logger cmp stat nproc \
-		   xfs_io modinfo blkdiscard realpath timeout" \
+		   xfs_io modinfo blkdiscard realpath timeout nvme" \
 	--include "$BLKTESTS_SRC" "/blktests" \
 	--include "$RAPIDO_DIR/autorun/blktests_zram.sh" "/.profile" \
 	--include "$RAPIDO_DIR/rapido.conf" "/rapido.conf" \
 	--include "$RAPIDO_DIR/vm_autorun.env" "/vm_autorun.env" \
-	--add-drivers "zram lzo lzo-rle scsi_debug null_blk loop" \
+	--add-drivers "zram lzo lzo-rle scsi_debug null_blk loop nvme nvme-loop" \
 	--modules "bash base" \
 	$DRACUT_EXTRA_ARGS \
 	$DRACUT_OUT || _fail "dracut failed"
