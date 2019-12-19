b427383a635f13addbe9e2740c447a59df336c81
David Disseldorp <ddiss@suse.de>
2019-12-19 15:20:23 +0100

    nvme_tcp_rbd: add cut and autorun scripts
    Map an RBD image and export it using the nvmet-tcp module.
    
    Signed-off-by: David Disseldorp <ddiss@suse.de>

---
+++ autorun/nvme_tcp_rbd.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#
+# Copyright (C) SUSE LLC 2019, all rights reserved.
+#
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation; either version 2.1 of the License, or
+# (at your option) version 3.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+# License for more details.
+
+if [ ! -f /vm_autorun.env ]; then
+	echo "Error: autorun scripts must be run from within an initramfs VM"
+	exit 1
+fi
+
+. /vm_autorun.env
+. /vm_ceph.env || _fatal
+
+set -x
+
+# map rbd device
+_vm_ar_rbd_map
+
+# this path is reliant on the rbd udev rule to setup the link
+CEPH_RBD_DEV=/dev/rbd/${CEPH_RBD_POOL}/${CEPH_RBD_IMAGE}
+[ -L $CEPH_RBD_DEV ] || _fatal
+
+modprobe configfs
+_vm_ar_configfs_mount
+
+modprobe nvme-core
+modprobe nvme-fabrics
+modprobe nvmet
+modprobe nvmet-tcp
+
+_vm_ar_dyn_debug_enable
+
+nvmet_subsys="nvmf-test"
+mkdir -p /sys/kernel/config/nvmet/subsystems/${nvmet_subsys} || _fatal
+cd /sys/kernel/config/nvmet/subsystems/${nvmet_subsys} || _fatal
+echo 1 > attr_allow_any_host || _fatal
+mkdir namespaces/1 || _fatal
+cd namespaces/1 || _fatal
+echo -n $CEPH_RBD_DEV > device_path || _fatal
+echo 1 > enable || _fatal
+
+mkdir /sys/kernel/config/nvmet/ports/1 || _fatal
+cd /sys/kernel/config/nvmet/ports/1 || _fatal
+
+echo "ipv4" > addr_adrfam || _fatal
+echo "tcp" > addr_trtype || _fatal
+echo "11345" > addr_trsvcid || _fatal
+echo "$NVME_TCP_TARGET" > addr_traddr || _fatal
+
+ln -s /sys/kernel/config/nvmet/subsystems/${nvmet_subsys} \
+	subsystems/${nvmet_subsys} || _fatal
+
+set +x
+
+echo "RBD mapped at: $CEPH_RBD_DEV, NVMe mapped"
---
+++ cut/nvme_tcp_rbd.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# Copyright (C) SUSE LLC 2019, all rights reserved.
+#
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation; either version 2.1 of the License, or
+# (at your option) version 3.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+# License for more details.
+
+RAPIDO_DIR="$(realpath -e ${0%/*})/.."
+. "${RAPIDO_DIR}/runtime.vars"
+
+vm_ceph_conf="$(mktemp --tmpdir vm_ceph_conf.XXXXX)"
+# remove tmp file once we're done
+trap "rm $vm_ceph_conf" 0 1 2 3 15
+
+_rt_require_ceph
+_rt_write_ceph_config $vm_ceph_conf
+_rt_require_dracut_args
+_rt_require_lib "libkeyutils.so.1"
+
+"$DRACUT" --install "tail blockdev ps rmdir resize dd vim grep find df sha256sum \
+		   strace mkfs.xfs ip ping \
+		   $LIBS_INSTALL_LIST" \
+	--include "$CEPH_CONF" "/etc/ceph/ceph.conf" \
+	--include "$CEPH_KEYRING" "/etc/ceph/keyring" \
+	--include "$RBD_NAMER_BIN" "/usr/bin/ceph-rbdnamer" \
+	--include "$RBD_UDEV_RULES" "/usr/lib/udev/rules.d/50-rbd.rules" \
+	--include "$RAPIDO_DIR/autorun/nvme_tcp_rbd.sh" "/.profile" \
+	--include "$RAPIDO_DIR/rapido.conf" "/rapido.conf" \
+	--include "$RAPIDO_DIR/vm_autorun.env" "/vm_autorun.env" \
+	--include "$vm_ceph_conf" "/vm_ceph.env" \
+	--add-drivers "nvme-core nvme-fabrics nvmet-tcp nvmet" \
+	--modules "bash base" \
+	$DRACUT_EXTRA_ARGS \
+	$DRACUT_OUT
