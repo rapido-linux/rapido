2f75b0631ae36398090d316c7fabe5775039c7ed
David Disseldorp <ddiss@suse.de>
2019-12-20 13:27:45 +0100

    nvme_tcp_initiator: add cut and autorun scripts
    Connect to the NVMe over TCP target specified via NVME_TARGET_TCP.
    
    Signed-off-by: David Disseldorp <ddiss@suse.de>

---
+++ cut/nvme_tcp_initiator.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Copyright (C) SUSE LLC 2019, all rights reserved.
+#
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation; either version 2.1 of the License, or
+# (at your option) version 3.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+# License for more details.
+
+RAPIDO_DIR="$(realpath -e ${0%/*})/.."
+. "${RAPIDO_DIR}/runtime.vars"
+
+_rt_require_dracut_args
+
+"$DRACUT" --install "tail blockdev ps rmdir resize dd vim grep find df sha256sum \
+		   strace mkfs.xfs ip ping" \
+	--include "$RAPIDO_DIR/autorun/nvme_tcp_initiator.sh" "/.profile" \
+	--include "$RAPIDO_DIR/rapido.conf" "/rapido.conf" \
+	--include "$RAPIDO_DIR/vm_autorun.env" "/vm_autorun.env" \
+	--include "$vm_ceph_conf" "/vm_ceph.env" \
+	--add-drivers "nvme-core nvme-fabrics nvme-tcp" \
+	--modules "bash base" \
+	$DRACUT_EXTRA_ARGS \
+	$DRACUT_OUT
