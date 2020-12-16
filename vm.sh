31f5b43b91fba7b670c7ffe60005df01c729d6ee
Samuel Cabrero <scabrero@suse.de>
2020-12-16 11:33:32 +0100

    vm: Add kernel parameter "net.ifnames=0" only to VMs with network
    Signed-off-by: Samuel Cabrero <scabrero@suse.de>
    Reviewed-by: David Disseldorp <ddiss@suse.de>

--- vm.sh
+++ vm.sh
@@ -35,7 +35,7 @@ _vm_start() {
 	# XXX rapido.conf VM parameters are pretty inconsistent and confusing
 	# moving to a VM${vm_num}_MAC_ADDR or ini style config would make sense
 	local qemu_netdev=""
-	local kern_net=""
+	local kern_net="net.ifnames=0"
 	if [[ -z $netd_flag ]]; then
 		# this image doesn't require network access
 		qemu_netdev="-net none"	# override default (-net nic -net user)
@@ -64,7 +64,7 @@ _vm_start() {
 		"${vm_resources[@]}" \
 		-kernel "$QEMU_KERNEL_IMG" \
 		-initrd "$DRACUT_OUT" \
-		-append "rapido.vm_num=${vm_num} net.ifnames=0 ${kern_net} \
+		-append "rapido.vm_num=${vm_num} ${kern_net} \
 			 rd.systemd.unit=emergency.target \
 		         rd.shell=1 console=$QEMU_KERNEL_CONSOLE rd.lvm=0 rd.luks=0 \
 			 $QEMU_EXTRA_KERNEL_PARAMS" \
