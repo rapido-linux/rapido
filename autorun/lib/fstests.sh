daa27595a54bfddd5daddbf315afb90f984ec346
David Disseldorp <ddiss@suse.de>
2022-01-09 23:10:55 +0100

    autorun/lib/fstests: add _fstests_devs_pool_provision
    Add any device with a serial matching a SCRATCH_DEV* wildcard to the
    fstests SCRATCH_DEV_POOL list, which is used for multi-device Btrfs
    testing.
    
    Signed-off-by: David Disseldorp <ddiss@suse.de>

--- autorun/lib/fstests.sh
+++ autorun/lib/fstests.sh
@@ -36,3 +36,39 @@ _fstests_devs_provision() {
 	done
 	unset _CFG
 }
+
+# same as _fstests_devs_provision() except a SCRATCH_DEV* wildcard is used to
+# fill SCRATCH_DEV_POOL. Any devices matching the wildcard will be added to the
+# SCRATCH_DEV_POOL list. If none are present then five zram devices will be
+# provisioned and used instead.
+_fstests_devs_pool_provision() {
+	local cfg_path="$1"
+	local i ser devp
+	declare -A _CFG=(["SCRATCH_LOGDEV"]="" ["SCRATCH_RTDEV"]=""
+			 ["TEST_DEV"]="")
+	declare -a _POOL=()
+
+	for i in $(ls /sys/block); do
+		ser="$(cat /sys/block/${i}/serial 2>/dev/null)" || continue
+		devp="/dev/${i}"
+		[[ -v "_CFG[$ser]" ]] && _CFG[$ser]="$devp"
+		[[ $ser == "SCRATCH_DEV"* && -b "$devp" ]] && _POOL+=("$devp")
+	done
+
+	[ -b "${_CFG[TEST_DEV]}" ] || _fstests_devs_zram_setup TEST_DEV
+	[ -b "${_CFG[SCRATCH_LOGDEV]}" ] || unset _CFG[SCRATCH_LOGDEV]
+	[ -b "${_CFG[SCRATCH_RTDEV]}" ] || unset _CFG[SCRATCH_RTDEV]
+	if [ ${#_POOL[@]} -eq 0 ]; then
+		for ((i = 0; i < 5; i++)); do
+			_fstests_devs_zram_setup POOL_DEV
+			_POOL+=("${_CFG[POOL_DEV]}")
+		done
+		unset _CFG[POOL_DEV]
+	fi
+	_CFG[SCRATCH_DEV_POOL]="${_POOL[@]}"
+
+	for i in "${!_CFG[@]}"; do
+		echo "${i}=\"${_CFG[$i]}\"" >> $cfg_path
+	done
+	unset _CFG _POOL
+}
