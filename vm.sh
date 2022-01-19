450b7ebf06eaa814098a145027b391a4e0be293f
David Disseldorp <ddiss@suse.de>
2022-01-19 10:26:35 +0100

    vm: use net-conf/vm# directories to configure network
    This works quite nicely and is quite expressive - tap devices are
    assigned to VMs based on the .network prefix in the corresponding vm#
    directory, allowing for multiple NICs per VM.
    
    MAC addresses (for netd [match]) are generated by taking an md5sum hash
    of the vm#.tapdev string.
    
    The removed net-conf/*.network examples will be replaced with a separate
    net-conf.example directory in future.
    
    Signed-off-by: David Disseldorp <ddiss@suse.de>

--- vm.sh
+++ vm.sh
@@ -19,9 +19,10 @@ _vm_is_running() {
 _vm_start() {
 	local vm_num=$1
 	local vm_pid_file="${RAPIDO_DIR}/initrds/rapido_vm${vm_num}.pid"
-	local netd_flag netd_mach_id kern_net
+	local netd_flag netd_mach_id kern_net i vm_tap tap_mac n
 	local vm_resources=()
 	local vm_num_kparam="rapido.vm_num=${vm_num}"
+	local qemu_netdev=()
 
 	[ -f "$DRACUT_OUT" ] \
 	   || _fail "no initramfs image at ${DRACUT_OUT}. Run \"cut_X\" script?"
@@ -33,12 +34,9 @@ _vm_start() {
 	_rt_qemu_resources_get "${DRACUT_OUT}" vm_resources netd_flag \
 		|| _fail "failed to get qemu resource parameters"
 
-	# XXX rapido.conf VM parameters are pretty inconsistent and confusing
-	# moving to a VM${vm_num}_MAC_ADDR or ini style config would make sense
-	local qemu_netdev=""
 	if [[ -z $netd_flag ]]; then
 		# this image doesn't require network access
-		qemu_netdev="-net none"	# override default (-net nic -net user)
+		qemu_netdev+=(-net none) # override default (-net nic -net user)
 		kern_net="rapido.networkless"
 	else
 		# networkd needs a hex unique ID (for dhcp leases, etc.)
@@ -47,17 +45,31 @@ _vm_start() {
 			|| _fail "failed to generate networkd machine-id"
 
 		kern_net="net.ifnames=0 systemd.machine_id=${netd_mach_id% *}"
-		eval local mac_addr='$MAC_ADDR'${vm_num}
-		[ -n "$mac_addr" ] \
-			|| _fail "MAC_ADDR${vm_num} not configured"
-		eval local tap='$TAP_DEV'$((vm_num - 1))
-		[ -n "$tap" ] \
-			|| _fail "TAP_DEV$((vm_num - 1)) not configured"
-		qemu_netdev="-device virtio-net,netdev=nw1,mac=${mac_addr} \
-			-netdev tap,id=nw1,script=no,downscript=no,ifname=${tap}"
-	fi
 
-	local qemu_more_args="$qemu_netdev $QEMU_EXTRA_ARGS"
+		[ -d "${RAPIDO_DIR}/net-conf/vm${vm_num}" ] \
+			|| _fail "net-conf/vm${vm_num} configuration missing"
+
+		n=0
+		for i in $(ls "${RAPIDO_DIR}/net-conf/vm${vm_num}"); do
+			[[ $i =~ ^(.*)\.network$ ]] || continue
+			vm_tap="${BASH_REMATCH[1]}"
+
+			# calculate a vNIC MAC based on the VM#
+			# and corresponding host tapdev name.
+			tap_mac=$(echo "vm${vm_num}.${vm_tap}" | md5sum | sed \
+			  's/^\(..\)\(..\)\(..\)\(..\)\(..\).*$/b8:\1:\2:\3:\4:\5/') \
+			  || _fail "failed to generate vm${vm_num}.${vm_tap} MAC"
+
+			# each entry is expected to match a corresponding tapdev
+			qemu_netdev+=(
+			  "-device"
+			  "virtio-net,netdev=if${n},mac=${tap_mac}"
+			  "-netdev"
+			  "tap,id=if${n},script=no,downscript=no,ifname=${vm_tap}"
+			)
+			((n++))
+		done
+	fi
 
 	# rapido.conf might have specified a shared folder for qemu
 	if [ -n "$VIRTFS_SHARE_PATH" ]; then
@@ -75,7 +87,8 @@ _vm_start() {
 		         rd.shell=1 console=$QEMU_KERNEL_CONSOLE rd.lvm=0 rd.luks=0 \
 			 $QEMU_EXTRA_KERNEL_PARAMS" \
 		-pidfile "$vm_pid_file" \
-		$qemu_more_args
+		"${qemu_netdev[@]}" \
+		$QEMU_EXTRA_ARGS
 	exit $?
 }
 
