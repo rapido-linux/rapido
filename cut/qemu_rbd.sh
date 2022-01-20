de2e40133972239207cb33afb70fb0b94b8ac3ef
David Disseldorp <ddiss@suse.de>
2022-01-20 11:04:07 +0100

    cut/qemu_rbd: replace _rt_xattr_qemu_args_set
    Use the new non-xattr based _rt_qemu_custom_args_set helper.
    
    Signed-off-by: David Disseldorp <ddiss@suse.de>

--- cut/qemu_rbd.sh
+++ cut/qemu_rbd.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-#
-# Copyright (C) SUSE LINUX GmbH 2016, all rights reserved.
-#
-# This library is free software; you can redistribute it and/or modify it
-# under the terms of the GNU Lesser General Public License as published
-# by the Free Software Foundation; either version 2.1 of the License, or
-# (at your option) version 3.
-#
-# This library is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
-# License for more details.
+# SPDX-License-Identifier: (LGPL-2.1 OR LGPL-3.0)
+# Copyright (C) SUSE LLC 2016-2022, all rights reserved.
 
 RAPIDO_DIR="$(realpath -e ${0%/*})/.."
 . "${RAPIDO_DIR}/runtime.vars"
 
+qemu_args_file="$(mktemp --tmpdir rbd_qemu_args.XXXXX)"
+trap "rm $qemu_args_file" 0 1 2 3 15
+
 _rt_require_dracut_args
 _rt_require_ceph
 _rt_require_lib "libkeyutils.so.1"
+
+cat >"$qemu_args_file" <<EOF
+-drive format=rbd,file=rbd:${CEPH_RBD_POOL}/${CEPH_RBD_IMAGE}:conf=${CEPH_CONF},if=virtio,cache=none,format=raw
+EOF
+# set qemu arguments to attach the RBD image. qemu uses librbd, and supports
+# writeback caching via a "cache=writeback" parameter.
+_rt_qemu_custom_args_set "$qemu_args_file"
 
 "$DRACUT" --install "tail blockdev ps rmdir resize dd vim grep find df sha256sum \
 		   strace mkfs.xfs lsscsi \
@@ -25,9 +25,3 @@ _rt_require_lib "libkeyutils.so.1"
 	--modules "base" \
 	"${DRACUT_RAPIDO_ARGS[@]}" \
 	"$DRACUT_OUT" || _fail "dracut failed"
-
-# set qemu arguments to attach the RBD image. qemu uses librbd, and supports
-# writeback caching via a "cache=writeback" parameter.
-qemu_cut_args="-drive format=rbd,file=rbd:${CEPH_RBD_POOL}/${CEPH_RBD_IMAGE}"
-qemu_cut_args="${qemu_cut_args}:conf=${CEPH_CONF},if=virtio,cache=none,format=raw"
-_rt_xattr_qemu_args_set "$DRACUT_OUT"  "$qemu_cut_args"
