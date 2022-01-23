7914345450e5a11a98ca8400ca5c412ae0e8aa99
David Disseldorp <ddiss@suse.de>
2022-01-23 22:38:48 +0100

    vm: drop rd.lvm and rd.luks kcli parameters
    These are processed by 90lvm and 90crypt Dracut modules respectively,
    which we don't include in any rapido images. Drop them.
    
    Signed-off-by: David Disseldorp <ddiss@suse.de>

--- vm.sh
+++ vm.sh
@@ -24,7 +24,7 @@ _vm_start() {
 	local vm_num_kparam="rapido.vm_num=${vm_num}"
 	local qemu_netdev=()
 	local kcmdline=(rd.systemd.unit=emergency.target \
-		rd.shell=1 "console=$QEMU_KERNEL_CONSOLE" rd.lvm=0 rd.luks=0 \
+		rd.shell=1 "console=$QEMU_KERNEL_CONSOLE" \
 		$QEMU_EXTRA_KERNEL_PARAMS)
 
 	[ -f "$DRACUT_OUT" ] \
