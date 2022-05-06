8120fe87c06f05007074e659577487706e7dd3d1
David Disseldorp <ddiss@suse.de>
2022-05-06 14:24:30 +0200

    nfsd_btrfs: kernel NFS server backed by Btrfs
    Start the kernel NFS server and export a zram+Btrfs backed share based
    on the NFS_SHARE rapido.conf setting.
    
    Signed-off-by: David Disseldorp <ddiss@suse.de>

---
+++ cut/nfsd_btrfs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# SPDX-License-Identifier: (LGPL-2.1 OR LGPL-3.0)
+# Copyright (C) SUSE LLC 2022, all rights reserved.
+
+RAPIDO_DIR="$(realpath -e ${0%/*})/.."
+. "${RAPIDO_DIR}/runtime.vars"
+
+_rt_require_dracut_args "$RAPIDO_DIR/autorun/nfsd_btrfs.sh" "$@"
+_rt_require_networking
+_rt_human_size_in_b "${FSTESTS_ZRAM_SIZE:-1G}" zram_bytes \
+	|| _fail "failed to calculate memory resources"
+_rt_mem_resources_set "$((2048 + (zram_bytes / 1048576)))M"
+
+"$DRACUT" --install "tail ps rmdir resize dd vim grep find df sha256sum \
+		   strace mkfs mkfs.btrfs stat which touch cut chmod \
+		   getfattr setfattr getfacl setfacl killall sync \
+		   id sort uniq date expr tac diff head dirname seq realpath \
+		   exportfs nfsdclddb nfsdclnts nfsdcltrack rcnfs-server \
+		   rpcbind rpc.mountd rpc.nfsd" \
+	--add-drivers "nfsd nfsv3 nfsv4 btrfs zram lzo lzo-rle" \
+	--modules "base" \
+	"${DRACUT_RAPIDO_ARGS[@]}" \
+	"$DRACUT_OUT" || _fail "dracut failed"
