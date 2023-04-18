cbfc1fd2074ec3893d7b2e7c75fd2ecd2f341dd0
David Disseldorp <ddiss@suse.de>
2023-04-18 18:31:16 +0200

    unionmount-testsuite: fix python deps and remove shutdown
    Drop the hardcoded python 3.6 path and instead pull all sys.path
    directories into the image.
    Also, drop the shutdown after the tests have been run, to allow for
    stashing of results, etc. The shutdown can easily be triggered via
    rapido cut -x "shutdown".
    
    Signed-off-by: David Disseldorp <ddiss@suse.de>

--- autorun/unionmount_testsuite.sh
+++ autorun/unionmount_testsuite.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
-#
+# SPDX-License-Identifier: (LGPL-2.1 OR LGPL-3.0)
 # Copyright (C) SUSE LLC 2021, all rights reserved.
-#
-# This library is free software; you can redistribute it and/or modify it
-# under the terms of the GNU Lesser General Public License as published
-# by the Free Software Foundation; either version 2.1 of the License, or
-# (at your option) version 3.
-#
-# This library is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
-# License for more details.
 
 _vm_ar_env_check || exit 1
 
@@ -44,4 +34,3 @@ for lowerfs in "xfs" "btrfs"; do
 	done
 done
 echo "all tests completed"
-shutdown
