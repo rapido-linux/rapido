f508117e89e553fe2d95fface2ce4a76cdc6e4f4
David Disseldorp <ddiss@suse.de>
2024-01-18 15:16:24 +1100

    cut/fstests_xfs: include gcc and header files for xfs/122
    xfs/122 uses the compiler to examine XFS type sizes. Use gcc -M in the
    cut script to obtain a recursive list of all dependencies for
    /usr/include/xfs/ headers.
    
    Signed-off-by: David Disseldorp <ddiss@suse.de>

--- cut/fstests_xfs.sh
+++ cut/fstests_xfs.sh
@@ -8,8 +8,8 @@ RAPIDO_DIR="$(realpath -e ${0%/*})/.."
 _rt_require_dracut_args "$RAPIDO_DIR/autorun/lib/fstests.sh" \
 			"$RAPIDO_DIR/autorun/fstests_xfs.sh" "$@"
 _rt_require_fstests
-pam_paths=()
-_rt_require_pam_mods pam_paths "pam_rootok.so" "pam_limits.so"
+req_inst=()
+_rt_require_pam_mods req_inst "pam_rootok.so" "pam_limits.so"
 _rt_human_size_in_b "${FSTESTS_ZRAM_SIZE:-1G}" zram_bytes \
 	|| _fail "failed to calculate memory resources"
 # 2x multiplier for one test and one scratch zram. +2G as buffer
@@ -21,6 +21,12 @@ man_deps=(man /etc/manpath.config \
 [[ ${man_deps[@]} == ${man_deps[@]%.bz2} ]] || man_deps+=(bzcat)
 [[ ${man_deps[@]} == ${man_deps[@]%.xz} ]] || man_deps+=(xzcat)
 
+# xfs/122: pull in compiler and xfs headers
+_rt_require_gcc req_inst stdio.h xfs/xfs.h xfs/xfs_types.h xfs/xfs_fs.h \
+	xfs/xfs_arch.h xfs/xfs_format.h xfs/linux.h
+# xfs/122: catch any headers which might be new
+req_inst+=("/usr/include/xfs/*.h")
+
 "$DRACUT" --install "tail blockdev ps rmdir resize dd vim grep find df sha256sum \
 		   strace mkfs mkfs.xfs free ip su uuidgen \
 		   which perl awk bc touch cut chmod true false unlink \
@@ -31,11 +37,11 @@ man_deps=(man /etc/manpath.config \
 		   dbench /usr/share/dbench/client.txt hostname getconf md5sum \
 		   od wc getfacl setfacl tr xargs sysctl link truncate quota \
 		   repquota setquota quotacheck quotaon pvremove vgremove \
-		   xfs_mkfile xfs_db xfs_io xfs_spaceman fsck \
+		   xfs_mkfile xfs_db xfs_io xfs_spaceman fsck comm indent \
 		   xfs_mdrestore xfs_bmap xfs_fsr xfsdump xfs_freeze xfs_info \
 		   xfs_logprint xfs_repair xfs_growfs xfs_quota xfs_metadump \
 		   chgrp du fgrep pgrep tar rev kill duperemove ${man_deps[*]} \
-		   ${pam_paths[*]} ${FSTESTS_SRC}/ltp/* ${FSTESTS_SRC}/src/* \
+		   ${req_inst[*]} ${FSTESTS_SRC}/ltp/* ${FSTESTS_SRC}/src/* \
 		   ${FSTESTS_SRC}/src/log-writes/* \
 		   ${FSTESTS_SRC}/src/aio-dio-regress/*" \
 	--include "$FSTESTS_SRC" "$FSTESTS_SRC" \
