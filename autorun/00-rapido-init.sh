8221e6ca4e6a45c0d4c32157964a1f5d60600004
David Disseldorp <ddiss@suse.de>
2024-05-28 07:23:51 +0200

    autorun/init: start vm_autorun.env via bash --rcfile
    Hanne's analysis:
      vm_autorun.env is copied into the rapido root fs as /etc/profile,
      and as such will be executed every time a login shell is invoked
      (ie via 'su -' or 'sh -l'). As this script also serves as system
      initialisation we need to make sure to run the initialisation only
      once, and only export the environment variables for later invocations.
    
    To avoid multiple invocations, we can use bash's explicit --rcfile
    parameter when starting the initial shell, and install vm_autorun.env
    in a path where it won't get invoked automatically (/rapido.rc).
    
    To use bash with --rcfile, we also need to drop the explicit --login/-l
    parameter, which changes bash behaviour slightly:
    - (unused) profile paths are different
    - legacy fcntl(fd=[3->19], F_SETFD, FD_CLOEXEC) calls are skipped
    
    I *think* we shouldn't see any side-effects for rapido workloads.
    
    Reported-by: Hannes Reinecke <hare@suse.de>
    Signed-off-by: David Disseldorp <ddiss@suse.de>

--- autorun/00-rapido-init.sh
+++ autorun/00-rapido-init.sh
@@ -1,15 +1,5 @@
-#
-# Copyright (C) SUSE LLC 2020, all rights reserved.
-#
-# This library is free software; you can redistribute it and/or modify it
-# under the terms of the GNU Lesser General Public License as published
-# by the Free Software Foundation; either version 2.1 of the License, or
-# (at your option) version 3.
-#
-# This library is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
-# License for more details.
+# SPDX-License-Identifier: (LGPL-2.1 OR LGPL-3.0)
+# Copyright (C) SUSE LLC 2020-2024, all rights reserved.
 
 # This script runs as the first Dracut entry point during /init.
 # $DRACUT_SYSTEMD is set when run via systemd.
@@ -24,7 +14,8 @@ for i in $(cat /proc/cmdline); do
 	esac
 done
 [ -c "$_ctty" ] || _ctty=/dev/tty1
-setsid --ctty /bin/sh -i -l 0<>$_ctty 1<>$_ctty 2<>$_ctty
+
+setsid --ctty -- /bin/bash --rcfile /rapido.rc -i 0<>$_ctty 1<>$_ctty 2<>$_ctty
 
 # shut down when rapido autorun / shell exits...
 echo 1 > /proc/sys/kernel/sysrq && echo o > /proc/sysrq-trigger
