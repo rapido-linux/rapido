bc327f3c090e79913bb603b2fa8f6d609f6ac896
David Disseldorp <ddiss@suse.de>
2025-06-11 11:12:17 +1000

    lkl: remount /dev/shm without noexec
    LKL MMU=y mode calls mmap() with PROT_EXEC, so the Dracut noexec default
    causes LKL boot failure if retained.
    
    Also add a few extra mkfs binaries for disk.sh -t $TYPE testing.
    
    Signed-off-by: David Disseldorp <ddiss@suse.de>

--- autorun/lkl_tests.sh
+++ autorun/lkl_tests.sh
@@ -11,6 +11,11 @@ _vm_ar_dyn_debug_enable
 
 set +x
 
+# Dracut mounts /dev/shm with noexec. drop it for arch/lkl/mm/mmu_mem.c
+# mmap_pages_for_ptes() which calls shmem_mmap() with PROT_EXEC:
+# https://github.com/lkl/linux/issues/606
+mount -t tmpfs -o remount,mode=1777,nosuid,nodev,strictatime tmpfs /dev/shm
+
 # USER used for net-setup.sh TAP_USER
 export USER=root
 cd "${LKL_SRC}/tools/lkl/tests"
