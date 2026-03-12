55f0caef6a85ccddbf823caeeb1d6fe50ed504a7
David Disseldorp <ddiss@suse.de>
2026-03-12 16:02:45 +1100

    rapido-init: drop /dev/shm and /run default tmpfs mounts
    I don't think we require these, and the /run mount means that we can't
    pack pre-created /run paths such as /run/systemd in the initramfs image
    without them disappearing.
    
    Signed-off-by: David Disseldorp <ddiss@suse.de>

--- src/bin/rapido-init.rs
+++ src/bin/rapido-init.rs
@@ -17,8 +17,6 @@ fn init_mount(do_debugfs: bool, do_virtfs: bool) -> io::Result<()> {
         ["-t", "sysfs", "-o", "nosuid,noexec,nodev", "sysfs", "/sys"],
         ["-t", "devtmpfs", "-o", "nosuid,noexec", "devtmpfs", "/dev"],
         ["-t", "devpts", "-o", "nosuid,noexec", "devpts", "/dev/pts"], // needed?
-        ["-t", "tmpfs", "-o", "mode=1777,noexec,nosuid,nodev", "tmpfs", "/dev/shm"], // needed?
-        ["-t", "tmpfs", "-o", "mode=755,noexec,nosuid,nodev", "tmpfs", "/run"],
         ["-t", "tmpfs", "-o", "noexec,nosuid,nodev", "tmpfs", "/tmp"],
     ];
     // sys_fsmount in the future, when we can do it without dependency bloat?
