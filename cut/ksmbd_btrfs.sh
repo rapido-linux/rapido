c2798a94655286c67a684fc8eda16852f8b71f1f
David Disseldorp <ddiss@suse.de>
2026-04-28 16:42:56 +1000

    cut/ksmbd_btrfs: use rapido-cut instead of dracut
    Benchmark: perf stat --repeat 10 ./rapido cut -B ksmbd-btrfs
    Before: 1.92916 +- 0.00756 seconds time elapsed  ( +-  0.39% )
    After: 0.04158 +- 0.00118 seconds time elapsed  ( +-  2.85% )
    
    Signed-off-by: David Disseldorp <ddiss@suse.de>

--- cut/ksmbd_btrfs.sh
+++ cut/ksmbd_btrfs.sh
@@ -1,28 +1,81 @@
 #!/bin/bash
 # SPDX-License-Identifier: (LGPL-2.1 OR LGPL-3.0)
-# Copyright (C) SUSE LLC 2023, all rights reserved.
+# Copyright (C) SUSE S.A. 2023-2026, all rights reserved.
 
 RAPIDO_DIR="$(realpath -e ${0%/*})/.."
 . "${RAPIDO_DIR}/runtime.vars"
 
-_rt_require_dracut_args "$RAPIDO_DIR/autorun/ksmbd_btrfs.sh" "$@"
-_rt_require_networking
 req_inst=()
 _rt_require_ksmbd_tools req_inst
 _rt_human_size_in_b "${FSTESTS_ZRAM_SIZE:-1G}" zram_bytes \
 	|| _fail "failed to calculate memory resources"
-_rt_mem_resources_set "$((1024 + (zram_bytes / 1048576)))M"
 _rt_require_conf_setting CIFS_USER CIFS_PW CIFS_SHARE
+printf -v req_inst_bins 'bin %s\n' "${req_inst[@]}"
 
-"$DRACUT" --install "tail ps rmdir resize dd grep find df sha256sum \
-		   strace mkfs mkfs.btrfs awk dirname \
-		   stat which touch cut chmod true false \
-		   getfattr setfattr getfacl setfacl killall sync \
-		   id sort uniq date expr tac diff head dirname seq \
-		   ${req_inst[*]}" \
-	--add-drivers "btrfs zram lzo lzo-rle \
-		       ksmbd ecb hmac md5 aes cmac sha256 sha512 ccm \
-		       gcm crc32" \
-	--modules "base" \
-	"${DRACUT_RAPIDO_ARGS[@]}" \
-	"$DRACUT_OUT" || _fail "dracut failed"
+PATH="target/release:${PATH}"
+rapido-cut --manifest /dev/stdin <<EOF
+file /rapido-rsc/mem/$((1024 + (zram_bytes / 1048576)))M
+include net.fest
+
+autorun autorun/ksmbd_btrfs.sh $*
+
+$req_inst_bins
+bin awk
+bin cat
+bin chmod
+bin cut
+bin date
+bin dd
+bin df
+bin diff
+bin dirname
+bin dirname
+bin expr
+bin false
+bin find
+bin getfacl
+bin getfattr
+bin grep
+bin head
+bin id
+bin killall
+bin ln
+bin ls
+bin mkdir
+bin mkfs
+bin mkfs.btrfs
+bin ps
+bin resize
+bin rmdir
+bin seq
+bin setfacl
+bin setfattr
+bin sha256sum
+bin sleep
+bin sort
+bin stat
+bin strace
+bin sync
+bin tac
+bin tail
+bin touch
+bin true
+bin uniq
+bin which
+
+kmod aes
+kmod btrfs
+kmod ccm
+kmod cmac
+kmod crc32
+kmod ecb
+kmod gcm
+kmod hmac
+kmod ksmbd
+kmod lzo
+kmod lzo-rle
+kmod md5
+kmod sha256
+kmod sha512
+kmod zram
+EOF
